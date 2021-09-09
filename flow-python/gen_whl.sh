b75a3123d00a1fc7ccfe07e84e6296e452fda26a
tpoisonooo <khj.application@aliyun.com>
2021-09-09 16:20:16 +0800

    docs(Python): rewrite whl pack script (#6)
    Co-authored-by: MegEngine <megengine@megvii.com>

--- flow-python/gen_whl.sh
+++ flow-python/gen_whl.sh
@@ -1,24 +1,23 @@
 # build py36~38 version
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate py36
 cargo build --example run_with_plugins --release
-cp ../target/release/examples/run_with_plugins ./pyflow/
-ldd pyflow/run_with_plugins
+cp ../target/release/examples/run_with_plugins ./pyflow/run_with_plugins_inner
 rm -rf ./build
-python3 whl-py36-setup.py bdist_wheel -p linux-x86_64 -d  py36_dist  --python-tag py36
+py='py36' python3 setup.py bdist_wheel -p linux-x86_64 -d  py36_dist
 
 conda activate py37
 cargo build --example run_with_plugins --release
-cp ../target/release/examples/run_with_plugins ./pyflow/
-ldd pyflow/run_with_plugins
+cp ../target/release/examples/run_with_plugins ./pyflow/run_with_plugins_inner
 rm -rf ./build
-python3 whl-py37-setup.py bdist_wheel -p linux-x86_64 -d  py37_dist  --python-tag py37
+py='py37' python3 setup.py bdist_wheel -p linux-x86_64 -d  py37_dist
 
 conda activate py38
 cargo build --example run_with_plugins --release
-cp ../target/release/examples/run_with_plugins ./pyflow/
-ldd pyflow/run_with_plugins
+cp ../target/release/examples/run_with_plugins ./pyflow/run_with_plugins_inner
 rm -rf ./build
-python3 whl-py38-setup.py bdist_wheel -p linux-x86_64 -d  py38_dist  --python-tag py38
+py='py38' python3 setup.py bdist_wheel -p linux-x86_64 -d  py38_dist
 
 
 rm -rf dist
@@ -26,3 +25,4 @@ mkdir dist
 cp py36_dist/pyflow-0.1.0-py36-none-linux_x86_64.whl dist/
 cp py37_dist/pyflow-0.1.0-py37-none-linux_x86_64.whl dist/
 cp py38_dist/pyflow-0.1.0-py38-none-linux_x86_64.whl dist/
+rm -rf py36_dist py37_dist py38_dist
--- flow-python/setup.py
+++ flow-python/setup.py
@@ -9,11 +9,47 @@
 
 #!/usr/bin/env python
 # coding=utf-8
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys
+import os
 
-setup(
-    name="pyflow",
-    version="0.1.0",
-    packages=["pyflow"],
-)
+if __name__ == '__main__':
+    py = os.getenv('py')
+    if py is None:
+        py = 'py{}{}'.format(sys.version_info[0], sys.version_info[1])
+    assert py.startswith('py3')
+    minor = int(py[-1])
+    setup(
+        options={'bdist_wheel':{'python_tag': py}},
+        name="pyflow",
+        version="0.1.0",
+        packages=["pyflow"],
+        author="Megvii IPU-SDK Team",
+        author_email="megengine@megvii.com",
+        url="https://github.com/MegEngine/MegFlow",
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Natural Language :: English',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Rust',
+            'Programming Language :: Python :: 3.{}'.format(minor),
+            'Topic :: Software Development :: Libraries :: Application Frameworks',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        python_requires='>=3.{},<3.{}'.format(minor, minor+1),
+        package_data={
+            "":['run_with_plugins_inner']
+        }, 
+        entry_points={
+            'console_scripts':['run_with_plugins=pyflow.command_line:main'],
+        },
+    )
 
